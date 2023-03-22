9d87e3b3718ea1035917ff66a641eeec5c5188b6
Yuliia1710 <113982788+Yuliia1710@users.noreply.github.com>
2023-03-22 22:00:45 +0000

    Deploying to gh-pages from @ Yuliia1710/goit-js-hw-09@fa4fb82be3c98a5ff5d8e10fb50d14292aa37f1b 🚀

--- 01-color-switcher.5f01e71e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]");console.log(t);const e=document.querySelector("button[data-stop]");console.log(e),t.addEventListener("click",(function(o){o.preventDefault(),t.disabled=!0,e.disabled=!1,timerColorId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(o){o.preventDefault(),clearInterval(timerColorId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.5f01e71e.js.map
